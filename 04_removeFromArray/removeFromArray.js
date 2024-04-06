9b2816ef7a9f451bd8d6b4277416f1c03085bb72
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-04-06 18:55:59 +0100

    Add functionality to removeFromArray
    Addfunctionality to allow the function to take
    an array as argument and remove all subsequent
    arguments from the array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let i = 0;
+    let new_array = [];
+    const argsArray = Array.from(arguments).slice(1);
+    while(i< array.length){
+        if(argsArray.includes(array[i])){
+            //dont add
+        }
+        else{
+            //do add
+            new_array.push(array[i]);
+        }
+        i++;
+    }
+    return new_array;
+}
 
-};
-
-// Do not edit below this line
+// Do not edit belowa this line
 module.exports = removeFromArray;
