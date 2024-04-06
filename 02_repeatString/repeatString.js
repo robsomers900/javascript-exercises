d6486ce71389ed14d4367a29eaf9065a83788c19
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-04-06 17:04:27 +0100

    Add Functionality to repeatString
    Addentire functionality to repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+// understand - write a function that takes two arguments - word and num - and returns the word num times with no spaces
+// plan - loop up to num, concating word to a str var each time
+// x 1 provide args
+// 2 create loop
+// 3 declare str var and += to word
 
+const repeatString = function(word, num) {
+    result = ""
+    if (num < 0){
+        return "ERROR";
+    }
+    else {    
+        for(i = 0; i < num; i++){
+        result += word;
+    }
+    return result;}
 };
 
 // Do not edit below this line
