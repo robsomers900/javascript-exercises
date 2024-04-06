bb0a6d87f86691e3178b30272f47518fa3587be6
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-04-06 17:44:12 +0100

    Created reverseString Function
    reverseString takes a string as argument and
    returns the same string in reverse. This
    functionality was added in this commit.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word){
+    wordArray = word.split("")
+    reversedWord = ""
+    i = 1
+    while(i <= wordArray.length){
+        reversedWord += wordArray.at(-i);
+        i++;
+    }
+    return reversedWord;
 
 };
 
