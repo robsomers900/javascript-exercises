1fca4c7a4349a03f8c2e649f6bb68ae2194f98ea
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-04-09 14:16:47 +0100

    Create leapYear Function
    leapYear takes a year and returns true if it is
    a year and false otherwise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+function isNumber(number){
+    return typeof number === 'year';
+}
 
+const leapYears = function(year) {
+    if (isNumber){
+        if (year % 400 == 0){
+            return true;
+        }
+        else if(year % 4 == 0 && year % 100 != 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else{
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
