79addbf294342ded3c6e5f072b11d78004c8d0c0
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-04-09 14:00:46 +0100

    Create sumAll Function
    sumAll takes 2 positive number inputs and sums all
    number inbetween and including.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+function isNumber(value) {
+    return typeof value === 'number';
+  }
 
+const sumAll = function(start, end) {
+    let sum = 0
+    if(start < 0){
+        return "ERROR";
+    }
+    else if(end < 0){
+        return "ERROR";
+    }
+    else if(isNumber(start ) == false || isNumber(end) == false){
+        return "ERROR";
+    }
+    else if(start <= end){
+        for(i = start; i <= end; i++){
+            sum += i;
+        }
+    }
+    else if(start > end){
+        for(i = end; i <= start; i++){
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
