71133bf567061965c686544e7d29ec5c07e621b6
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-04-30 14:00:55 +0100

    Add functionality to calculator functions
    - Added all functionality
    - Removed .skip method from all tests in spec file

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, currentValue) => {
+    return total + currentValue;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currentValue) => {
+    return total * currentValue;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	arr = []
+  for(let i = 1; i <= num; i++) {
+    arr.push(i);
+  }
+  return multiply(arr);
 };
 
 // Do not edit below this line
