7542a28d2098744bdec80fdb9e8d4ea5e18a448e
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-04-30 14:03:31 +0100

    No changes to js and spec files

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsisus = (fahrenheit - 32)/(5/9);
+  return Math.round(celsisus*10/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = celsisus*(9/5) + 32;
+  return Math.round(fahrenheit*10)/10;
 };
 
 // Do not edit below this line
