4a8b56be88358fa9357b8775e47a85008e5d51b1
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-05-01 14:27:36 +0100

    Add functionality to palindrom function
    - Added functionality to function
    - Removed .skip functions from tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    array = str.split('');
+    array = array.filter(char => {
+        const regex = /[^\w\s]/;
+        //char not equal to space or special
+        if(regex.test(char) != true && char != " "){
+            return true;
+        }
+        else{
+            return false;
+        };
+    });
+    reversedArray = array.slice().reverse();
+    reversedStr = reversedArray.join('');
+    str = array.join('');
+    //return reversedArray
+    
+    if (reversedStr == str){
+        return true
+    }
+    else {
+        return false;
+    }
 };
+//console.log(palindromes("Animal loots foliated detail of stool lamina."))
 
 // Do not edit below this line
 module.exports = palindromes;
