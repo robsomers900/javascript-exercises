abd86b9565bd17fe7a1c4b8d8e16a1a3ea27dcdb
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-05-07 14:10:43 +0100

    Add functionality to findTheOldest
    - Added functionality to the function
    - Removed .skip from test cases

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestName = "";
+    let age = 0
+    for (const person of people){
+        age = person['yearOfDeath'] - person['yearOfBirth'];
+        if (age > oldestAge){
+            oldestAge = age;
+            oldestName = person['name'];
+        }
+    }
+    return oldestName;
 };
 
-// Do not edit below this line
 module.exports = findTheOldest;
