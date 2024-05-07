e122c7d7d3358a132083cfa30a2e9afea5420f8c
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-05-07 13:37:58 +0100

    Add functionality to fibonacci function
    - Added functionality to fibonacci function
    - Updated test script

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num)
+    if (num < 0){
+        return "OOPS";
+    }
+    else if (num < 2 && num >= 0){
+        return num;
+    }
+    else {
+        let now = 0;
+        let next = 1;
+        arr = []
+        arr[0] = 0;
+        for(i = 1; i < num; i ++){
+            temp = next + now;
+            arr[i] = temp;
+            now = next;
+            next = temp;
+        }
+        return arr[num - 1];
+    };
 };
+
+//console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
