1c6cc7d08536af5fdcb5343c6678b37cb95adf72
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-05-07 13:53:47 +0100

    Add functionality to getTheTitles function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(inputArr) {
+    let returnArr = [];
+    for (const book of inputArr){
+        returnArr.push(book['title']);
+    }
+    return returnArr;
 };
+// console.log(getTheTitles([      
+//     {
+//     title: 'Book',
+//     author: 'Name'
+//     },
+//     {
+//     title: 'Book2',
+//     author: 'Name2'
+//   }]))
 
 // Do not edit below this line
 module.exports = getTheTitles;
